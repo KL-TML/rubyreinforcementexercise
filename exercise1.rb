0f7853a969f74e7b036eada7e1cae25d0d72bdbf
KYLE LIPTON <lipton.kyle@gmail.com>
2017-10-31 19:09:23 -0400

    exercise 1

---
+++ exercise1.rb
@@ -0,0 +1,35 @@
+# list of films
+documentary = "Supersize Me"
+drama = "Girl on a Train"
+comedy = "Dumb and Dumber"
+dramedy = "Dragon tattoo"
+
+
+puts "Do you like documentaries? (Yes/No)"
+  doc_input = gets.chomp
+puts "Do you enjoy dramas? (Yes/No)"
+  drama_input = gets.chomp
+puts "Do you enjoy comedies? (Yes/No)"
+  comedy_input = gets.chomp
+
+  # if the answer yes to documentaries
+  if doc_input = "Yes"
+    puts "You will like #{documentary}!"
+
+  # If answer yes to dramedy
+  elsif doc_input == "No" && drama_input == "Yes" && comedy_input = "Yes"
+    puts "You will love #{dramedy}!"
+
+  # if answer yes to drama
+  elsif doc_input == "No" && drama_input = "Yes" && comedy_input == "No"
+    puts "You will love #{drama}"
+
+  # if answer yes to comedy
+  elsif doc_input == "No" && drama_input = "No" && comedy_input == "Yes"
+    puts "You will love #{comedy}"
+
+  # if answer no to all three - (answer yes to a book)
+  elsif doc_input == "No" && drama_input = "No" && comedy_input == "No"
+    puts "Go Read a Book!"
+
+  end
