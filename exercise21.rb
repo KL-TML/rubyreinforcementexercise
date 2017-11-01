a026d93a38fff1dc05294860b623c0f2ff351ead
KYLE LIPTON <lipton.kyle@gmail.com>
2017-11-01 15:19:12 -0400

    exercise 1 complete

---
+++ exercise21.rb
@@ -0,0 +1,7 @@
+def double(my_number)
+  return my_number * 2
+end
+
+puts "This method results in 2 x 2 equals to #{double(2)}"
+puts "This method results in 3 x 2 equals to #{double(3)}"
+puts "This method results in 4 x 2 equals to #{double(4)}"
