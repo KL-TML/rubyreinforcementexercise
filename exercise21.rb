e067eda47ce111ac29184821126578ea429f154b
KYLE LIPTON <lipton.kyle@gmail.com>
2017-11-01 15:28:01 -0400

    revised exercise1

--- exercise21.rb
+++ exercise21.rb
@@ -5,4 +5,3 @@ end
 puts "This method results in 2 x 2 equals to #{double(2)}"
 puts "This method results in 3 x 2 equals to #{double(3)}"
 puts "This method results in 4 x 2 equals to #{double(4)}"
-puts 11
